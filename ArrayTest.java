7b1a02a75bc0c83efa1e382cc83b0e4a5fd8544e
fkhan00 <43051637+fkhan00@users.noreply.github.com>
2018-12-19 13:48:15 -0500

    Branch

---
+++ ArrayTest.java
@@ -0,0 +1,11 @@
+public class ArrayTest{
+   public static void main(String[]args){
+	String List = "{}"
+	int[] Array = new int[10];
+        for(i = 0; i < 10; i ++){
+		Array[i] = (i + 1) * 100;
+		Array[i] 
+	}
+   }
+
+}
